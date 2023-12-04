9d4b068fb5553fca20a95e5024a57a487cf7a083
Mrlucioz <billemjobs@gmail.com>
2023-12-03 22:20:41 -0600

    jajaj bye bye

---
+++ jaja/hello2.py
@@ -0,0 +1,17 @@
+print("hello world!")
+x =45450
+comemierda= "sinonimo"
+
+
+#fjsjdjfs
+if x<10000 and x>4000:
+    print("pos si jajja ")
+elif x<10000:
+
+    print ("me cago jaja ")
+else:
+    print("ninguno")
+
+    def me_cago():
+        print("jajaj")
+        print("fjsdljfs")
--- jaja/readme.md
+++ jaja/readme.md
@@ -1,1 +1,2 @@
-this is the readme
+ththis is the readme
+th
